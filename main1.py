3c6ade4f358debc8cb74d708b0924b9d1d296bdb
Ayushi Mittal(she/her) <54371479+AyushiNM@users.noreply.github.com>
2022-02-25 17:46:42 +0530

    ASL Recoginition

---
+++ main1.py
@@ -0,0 +1,39 @@
+import infer
+import streamlit as st
+from PIL import Image
+import camera
+from gtts import gTTS
+import os
+
+def main():
+    st.title("ASL Recognization APP")
+
+    uploaded_file = st.file_uploader("Upload an image here -")
+    label = ""
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Alphabet.', width=200)
+        st.write("")
+        label = infer.predict(image)
+
+        st.write("Inference Done!")
+        output = "Label = " + label
+        st.write(output)
+
+    print("Inference Done!")
+    print("Label = ", label)
+    mytext = 'Identified Label is {}'.format(label)
+  
+
+    language = 'en'
+
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+
+    myobj.save("voice.mp3")
+  
+
+    os.system("voice.mp3")
+
+
+if __name__ == '__main__':
+    main()
